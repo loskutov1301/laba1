5f3a625b090e3b0839cc31aa4cff68b88ad3891d
loskutov1301 <95621112+loskutov1301@users.noreply.github.com>
2021-12-06 14:04:07 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.gibo01.pr1;
+
+public class Main {
+    public static void main (String[] args) {
+        Sofa divan1 = new Sofa ( 150, 50, 80, "yellow");
+        System.out.println(divan1);
+    }
+}
